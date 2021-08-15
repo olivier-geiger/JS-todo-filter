fabd0df5cb801e99b6b2568021f9b3e50f054d13
olivier-geiger <69504966+olivier-geiger@users.noreply.github.com>
2021-08-15 09:32:21 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+  list.innerHTML += html;
+};
+
+const filterTodos = searchInput => {
+  Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(searchInput))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(searchInput))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+
+// add todos
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  console.log(todo);
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// delete todos
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+});
+
+// filter todos
+search.addEventListener('keyup', () => {
+  const inputSearch = search.value.trim().toLowerCase();
+
+  filterTodos(inputSearch);
+});
